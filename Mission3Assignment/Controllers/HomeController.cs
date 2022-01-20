7e7d6e2de1cbd5870155d5f5f2dff8aee6d98697
John Fluhman <john.fluhman@gmail.com>
2022-01-19 20:37:39 -0700

    Initial Commit

---
+++ Mission3Assignment/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mission3Assignment.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Mission3Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalculator(GradeCalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission3Assignment/wwwroot/js/grade_calculator.js
@@ -0,0 +1,44 @@
+﻿$('#btnSend').click(function () {
+    // Pulls the values from the html form for each of the grade types
+    var assingments = parseInt($('#assignments').val(), 10);
+    var gp = parseInt($('#groupProject').val(), 10);
+    var quizzes = parseInt($('#quizzes').val(), 10);
+    var exams = parseInt($('#exams').val(), 10);
+    var intex = parseInt($('#intex').val(), 10);
+
+    // Calculates the final grade based on syllabus weights and creates a string for the alert to be used later
+    var final_grade = (assingments * .55) + (gp * .05) + (quizzes * .1) + (exams * .2) + (intex * .1);
+    var final_grade_string = final_grade.toString();
+
+    // Calculates the letter graded based on the weighted final grade
+    if (final_grade >= 93) {
+        var letter_grade = 'A'
+    } else if (final_grade >= 90) {
+        var letter_grade = 'A-'
+    } else if (final_grade >= 87) {
+        var letter_grade = 'B+'
+    } else if (final_grade >= 83) {
+        var letter_grade = 'B'
+    } else if (final_grade >= 80) {
+        var letter_grade = 'B-'
+    } else if (final_grade >= 77) {
+        var letter_grade = 'C+'
+    } else if (final_grade >= 73) {
+        var letter_grade = 'C'
+    } else if (final_grade >= 70) {
+        var letter_grade = 'C-'
+    } else if (final_grade >= 67) {
+        var letter_grade = 'D+'
+    } else if (final_grade >= 63) {
+        var letter_grade = 'D'
+    } else if (final_grade >= 60) {
+        var letter_grade = 'D-'
+    } else {
+        var letter_grade = 'E'
+    }
+         
+    // Sends the final grade calculations to the user via Alert
+    alert(final_grade_string + '% Letter Grade: ' + letter_grade);
+})
+
+
